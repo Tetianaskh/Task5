aa1154c3ff76d38ea71ecf968befc42db5445e7d
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-10 21:47:28 +0200

    Initial commit

---
+++ src/com/company/Task5.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Task5 {
+
+   // Основные задачи:
+
+    // Задача 1.
+    public static void exercise01() {
+
+    }
+
+    // Задача 2.
+    public static void exercise02() {
+
+    }
+
+    // Задача 3.
+    public static void exercise03() {
+
+    }
+
+    // Дополнительные задачи:
+
+    // Задача 1.
+    public static void exercise001() {
+
+    }
+
+    // Задача 2.
+    public static void exercise002() {
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
