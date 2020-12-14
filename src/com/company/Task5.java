e6cce70f30031a0988d03d2dbb53ba5194894ad8
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-14 16:48:55 +0200

    Основные задачи: 1 + 2 + 3 Дополнительные задачи: 001 + 002

--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Task5 {
 
-   // Основные задачи:
+    // Основные задачи:
 
-    // Задача 1. Используя цикл for вывести те числа которые делятся на 5, но не делятся на 3 из интервала 1...100
+    /**
+     * Задача 1. Используя цикл for вывести те числа которые делятся на 5, но не делятся на 3 из интервала 1...100
+     */
     public static void exercise01() {
         System.out.println("Добрый день!");
         System.out.println("Наш помощник выведет числа которые делятся на 5 но не делятся на 3 из интерала 1...100:");
@@ -17,13 +19,16 @@ public class Task5 {
         }
     }
 
-    // Задача 2. Вывести все простые числа из интервала 1...100
-    /***
+    /**
      * Проверяет делится ли число на другие числа кроме 1 и самого себя
-     * @param number - число от 1 до 100
-     * @return - простое число или нет
+     *
+     * @param number число от 1 до 100
+     * @return простое число или нет
      */
-    public static boolean isPrimeNumber (int number) {
+    public static boolean isPrimeNumber(int number) {
+        if (number == 1) {
+            return false;
+        }
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
@@ -32,6 +37,9 @@ public class Task5 {
         return true;
     }
 
+    /**
+     * Задача 2. Вывести все простые числа из интервала 1...100
+     */
     public static void exercise02() {
         System.out.println("Добрый день!");
         System.out.println("Наш помощник выведет все простые числа из интервала 1...100:");
@@ -42,10 +50,14 @@ public class Task5 {
         }
     }
 
-    // Задача 3. Пользователь поочередно вводит числа, пока не введет 0, найти их среднее арифметичесткое, использовать break
+    /**
+     * Задача 3. Пользователь поочередно вводит числа, пока не введет 0, найти их среднее арифметичесткое,
+     * использовать break
+     */
     public static void exercise03() {
         System.out.println("Добрый день!");
-        System.out.println("Наш помощник посчитает среднее арифметическое всех введенных Вами чисел (до введенного числа 0):");
+        System.out.println("Наш помощник посчитает среднее арифметическое всех введенных Вами чисел " +
+                "(до введенного числа 0):");
         Scanner sc = new Scanner(System.in);
         int number;
         int i = 0;
@@ -66,15 +78,14 @@ public class Task5 {
 
     // Дополнительные задачи:
 
-    // Задача 1. Написать метод, который принимает 2 целых числа и возвращает их наибольший общий делитель
-
-    /***
+    /**
      * Вычисление наибольшего общего делителя двух целых чисел
-      * @param number1 - первое число вводимое пользователем
-     * @param number2 - второе число вводимое пользователем
-     * @return -
+     *
+     * @param number1 первое число
+     * @param number2 второе число
+     * @return наибольший общий делитель
      */
-    public static int getGreatestCommonFactor (int number1, int number2) {
+    public static int getGreatestCommonDivisor(int number1, int number2) {
         while (number2 != 0) {
             int remainderOfDivision = number1 % number2;
             number1 = number2;
@@ -83,6 +94,9 @@ public class Task5 {
         return number1;
     }
 
+    /**
+     * Задача 1. Написать метод, который принимает 2 целых числа и возвращает их наибольший общий делитель
+     */
     public static void exercise001() {
         System.out.println("Добрый день!");
         System.out.println("Наш помощник посчитает наибольший общий делитель двух целых чисел:");
@@ -90,20 +104,23 @@ public class Task5 {
         System.out.println("Введите, пожалуйста, два целых числа:");
         int number1 = sc.nextInt();
         int number2 = sc.nextInt();
-        System.out.println("Наибольший общий делитель чисел " + number1 + " и " + number2 + " = " + getGreatestCommonFactor(number1, number2));
+        int commonDivisor = getGreatestCommonDivisor(number1, number2);
+        System.out.println("Наибольший общий делитель чисел " + number1 + " и " + number2 + " = " + commonDivisor);
     }
 
-    // Задача 2. Пользователь вводит число описывающее маршрут? где каждая цифра означает направление
-    // 1 - прямо, 2 - направо, 3 - налево, вывести этот маршрут текстом, причем если единица повторяется
-    // вывести "продолжайте движение прямо"
-    // Например? если пользователь введет 1211312
-    // двигайтесь прямо
-    // поверните направо
-    // двигайтесь прямо
-    // продолжайте движение прямо
-    // поверните налево
-    // двигайтесь прямо
-    // повернитесь направо
+    /*
+     *    Задача 2. Пользователь вводит число описывающее маршрут? где каждая цифра означает направление
+     *   1 - прямо, 2 - направо, 3 - налево, вывести этот маршрут текстом, причем если единица повторяется
+     *    вывести "продолжайте движение прямо"
+     *    Например? если пользователь введет 1211312
+     *    двигайтесь прямо
+     *    поверните направо
+     *    двигайтесь прямо
+     *    продолжайте движение прямо
+     *    поверните налево
+     *    двигайтесь прямо
+     *    повернитесь направо
+     */
     public static void exercise002() {
         System.out.println("Добрый день!");
         System.out.println("Наш помощник опишет маршрут из введенных вами чисел:");
@@ -112,10 +129,11 @@ public class Task5 {
         String route = sc.nextLine();
         for (int i = 0; i < route.length(); i++) {
             if (route.charAt(i) == '1') {
-                if (route.charAt(i - 1) == '1') {
+                if (i > 0 && route.charAt(i - 1) == '1') {
                     System.out.println("Продолжайте движение прямо");
-                } else System.out.println("Двигайтесь прямо");
-
+                } else {
+                    System.out.println("Двигайтесь прямо");
+                }
             } else if (route.charAt(i) == '2') {
                 System.out.println("Поверните направо");
             } else if (route.charAt(i) == '3') {
